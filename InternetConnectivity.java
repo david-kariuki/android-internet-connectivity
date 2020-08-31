7654949f3152ca9f2731ed849c364aec0571a3e0
David Kariuki <dkaris.k@gmail.com>
2020-08-31 05:58:59 +0300

    Update InternetConnectivity.java

--- InternetConnectivity.java
+++ InternetConnectivity.java
@@ -23,95 +23,121 @@ import java.util.Objects;
 /**
  * This class checks for device's network connectivity and the network speed
  * WiFi network speed is calculated by downloading a file and calculating the network speed
+ *
  * @author David Kariuki
  */
-@SuppressWarnings("unused")
+@SuppressWarnings("RedundantSuppression")
 public class InternetConnectivity {
 
     @SuppressWarnings("unused")
-    private static final String TAG = InternetConnectivity.class.getSimpleName(); // Get activity simple name
+    // Get activity simple name
+    private static final String TAG = InternetConnectivity.class.getSimpleName();
     private static final int byteValue = 1000;
-
     private static HashMap<String, String> connectionSpeedInfo;
 
     /**
      * Check for internet connection
+     *
      * @param context - For getting network info
      * @return boolean
      */
-    public static boolean isConnectedToAnyNetwork(Context context){
+    public static boolean isConnectedToAnyNetwork(Context context) {
         NetworkInfo info = InternetConnectivity.getNetworkInfo(context);
         return (info != null && info.isConnected());
     }
 
     /**
      * Check for WiFi network connection
+     *
      * @param context - For getting network info
      * @return return
      */
-    public static boolean isConnectedToWifiNetwork(Context context){
+    public static boolean isConnectedToWifiNetwork(Context context) {
         NetworkInfo info = InternetConnectivity.getNetworkInfo(context);
-        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+        return (info != null && info.isConnected() && info.getType() ==
+                ConnectivityManager.TYPE_WIFI);
     }
 
     /**
      * Check for Mobile network connection
+     *
      * @param context - For getting network info
      * @return boolean
      */
-    public static boolean isConnectedToMobileNetwork(Context context){
+    public static boolean isConnectedToMobileNetwork(Context context) {
         NetworkInfo info = InternetConnectivity.getNetworkInfo(context);
-        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+        return (info != null && info.isConnected() && info.getType() ==
+                ConnectivityManager.TYPE_MOBILE);
     }
 
     /**
      * Check for VPN network connection
+     *
      * @param context - For getting network info
      * @return boolean
      */
-    public static boolean isConnectedToVPNNetwork(Context context){
+    public static boolean isConnectedToVPNNetwork(Context context) {
         NetworkInfo info = InternetConnectivity.getNetworkInfo(context);
-        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_VPN);
+        return (info != null && info.isConnected() && info.getType() ==
+                ConnectivityManager.TYPE_VPN);
     }
 
     /**
      * Check for fast connection
+     *
      * @param context - For getting network info
      * @return boolean
      */
-    public static boolean isConnectionFast(Context context){
+    public static boolean isConnectionFast(Context context) {
         NetworkInfo info = InternetConnectivity.getNetworkInfo(context);
-        return (info != null && info.isConnected() && InternetConnectivity.isConnectionFast(info.getType(),info.getSubtype()));
+        return (info != null && info.isConnected() &&
+                InternetConnectivity.isConnectionFast(info.getType(), info.getSubtype()));
     }
 
     /**
      * Check for fast connection with specific type and subType
-     * @param type - networkType
+     *
+     * @param type    - networkType
      * @param subType - networkSubType
      * @return boolean
      */
-    public static boolean isConnectionFast(int type, int subType){
-        if (type==ConnectivityManager.TYPE_WIFI || type==ConnectivityManager.TYPE_VPN){
+    public static boolean isConnectionFast(int type, int subType) {
+        if (type == ConnectivityManager.TYPE_WIFI || type == ConnectivityManager.TYPE_VPN) {
             // Calculate WiFi network speed - (WiFi is not always fast)
             return calculateNetworkSpeedByDownloadingFile();
-        } else if(type==ConnectivityManager.TYPE_MOBILE){
-            switch(subType){
+        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+            switch (subType) {
                 case TelephonyManager.NETWORK_TYPE_1xRTT:
-                case TelephonyManager.NETWORK_TYPE_EDGE:    return false; // ~ 50-100     -Kbps - slow.
-                case TelephonyManager.NETWORK_TYPE_CDMA:    return false; // ~ 14-64      -Kbps - slow.
-                case TelephonyManager.NETWORK_TYPE_EVDO_0:  return true;  // ~ 400-1000   -Kbps
-                case TelephonyManager.NETWORK_TYPE_EVDO_A:  return true;  // ~ 600-1400   -Kbps
-                case TelephonyManager.NETWORK_TYPE_GPRS:    return false; // ~ 100        -Kbps - slow.
-                case TelephonyManager.NETWORK_TYPE_HSDPA:   return true;  // ~ 2000-1400  -Kbps
-                case TelephonyManager.NETWORK_TYPE_HSPA:    return true;  // ~ 700-1700   -Kbps
-                case TelephonyManager.NETWORK_TYPE_HSUPA:   return true;  // ~ 1000-2300  -Kbps
-                case TelephonyManager.NETWORK_TYPE_UMTS:    return true;  // ~ 400-7000   -Kbps
-                case TelephonyManager.NETWORK_TYPE_EHRPD:   return true;  // ~ 1000-2000  -Kbps - API level 11
-                case TelephonyManager.NETWORK_TYPE_EVDO_B:  return true;  // ~ 5000       -Kbps - API level 9
-                case TelephonyManager.NETWORK_TYPE_HSPAP:   return true;  // ~ 10000-20000-Kbps - API level 13
-                case TelephonyManager.NETWORK_TYPE_IDEN:    return false; // ~ 25         -Kbps - API level 8
-                case TelephonyManager.NETWORK_TYPE_LTE:     return true;  // ~ 10000+     -Kbps - API level 11
-                case TelephonyManager.NETWORK_TYPE_UNKNOWN:               // ~ Unknown
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                    return false; // ~ 50-100    Kbps - slow.
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                    return false; // ~ 14-64     Kbps - slow.
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                    return true; // ~ 400-1000  Kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                    return true; // ~ 600-1400  Kbps
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                    return false; // ~ 100       Kbps - slow.
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                    return true; // ~ 2000-1400 Kbps
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                    return true; // ~ 700-1700  Kbps
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                    return true; // ~ 1000-2300 Kbps
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                    return true; // ~ 400-7000  Kbps
+                case TelephonyManager.NETWORK_TYPE_EHRPD:
+                    return true; // ~ 1000-2000 Kbps - API level 11
+                case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                    return true; // ~ 5000      Kbps - API level 9
+                case TelephonyManager.NETWORK_TYPE_HSPAP:
+                    return true; // ~ 10000-20000Kbps - API level 13
+                case TelephonyManager.NETWORK_TYPE_IDEN:
+                    return false; // ~ 25        Kbps - API level 8
+                case TelephonyManager.NETWORK_TYPE_LTE:
+                    return true; // ~ 10000+    Kbps - API level 11
+                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                    return false; // ~ Unknown
                 default:
                     return false;
             }
@@ -122,8 +148,11 @@ public class InternetConnectivity {
 
     /**
      * Check for fast WiFi connection
-     * Returns detailed information like time taken in milliseconds, seconds, networks speed (in bytes, kbps and mbps), download speed and test file size.
-     * @return HashMap<String, String> - downloadResponse information for debugging purposes or more specific checks
+     * Returns detailed information like time taken in milliseconds, seconds, networks speed
+     * (in bytes, kbps and mbps), download speed and test file size.
+     *
+     * @return HashMap<String, String> - downloadResponse information for debugging purposes or
+     * more specific checks
      */
     public static HashMap<String, String> getWiFiConnectionSpeedInfo(Context context) {
         if (getNetworkInfo(context).getType() == ConnectivityManager.TYPE_WIFI) {
@@ -134,8 +163,11 @@ public class InternetConnectivity {
 
     /**
      * Check for fast VPN connectio.n
-     * Returns detailed information like time taken in milliseconds, seconds, networks speed (in bytes, kbps and mbps), download speed and test file size.
-     * @return HashMap<String, String> - downloadResponse information for debugging purposes or more specific checks
+     * Returns detailed information like time taken in milliseconds, seconds, networks speed
+     * (in bytes, kbps and mbps), download speed and test file size.
+     *
+     * @return HashMap<String, String> - downloadResponse information for debugging purposes or
+     * more specific checks
      */
     public static HashMap<String, String> getVPNConnectionSpeedInfo(Context context) {
         if (getNetworkInfo(context).getType() == ConnectivityManager.TYPE_VPN) {
@@ -146,19 +178,22 @@ public class InternetConnectivity {
 
     /**
      * Get network info
+     *
      * @param context - For getting connectivity service
      * @return NetworkInfo
      */
-    private static NetworkInfo getNetworkInfo(Context context){
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    private static NetworkInfo getNetworkInfo(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return Objects.requireNonNull(connectivityManager).getActiveNetworkInfo();
     }
 
     /**
      * Calculate network speed by downloading file
+     *
      * @return networkState
      */
-    private static boolean calculateNetworkSpeedByDownloadingFile(){
+    private static boolean calculateNetworkSpeedByDownloadingFile() {
 
         long startTime;
         final long[] endTime = new long[1];
@@ -168,7 +203,9 @@ public class InternetConnectivity {
 
         // Create and build request
         Request request = new Request.Builder()
-                .url("https://github.com/david-kariuki/AndroidInternetConnectivity/blob/master/test_download_image.png") // Url of image or file to be downloaded
+                // Url of image or file to be downloaded
+                .url("https://github.com/david-kariuki/AndroidInternetConnectivity/blob/master" +
+                        "/test_download_image.png")
                 .build(); // Build request
 
         // Get start time in milli seconds
@@ -187,13 +224,23 @@ public class InternetConnectivity {
 
                     // Switch error codes
                     switch (response.code()) {
-                        case 401:   throw new IOException("Unauthorized " + response.code());
-                        case 404:   throw new IOException("Not found " + response.code());
-                        case 408:   throw new IOException("Request Timeout " + response.code());
-                        case 429:   throw new IOException("Too Many Requests " + response.code());
-                        case 444:   throw new IOException("Connection Closed Without Response " + response.code());
-                        case 500:   throw new IOException("Internal Server Error " + response.code());
-                        default:    throw new IOException("Unexpected code " + response.code() + " "  + response);
+                        case 401:
+                            throw new IOException("Unauthorized " + response.code());
+                        case 404:
+                            throw new IOException("Not found " + response.code());
+                        case 408:
+                            throw new IOException("Request Timeout " + response.code());
+                        case 429:
+                            throw new IOException("Too Many Requests " + response.code());
+                        case 444:
+                            throw new IOException("Connection Closed Without Response "
+                                    + response.code());
+                        case 500:
+                            throw new IOException("Internal Server Error "
+                                    + response.code());
+                        default:
+                            throw new IOException("Unexpected code " + response.code()
+                                    + " " + response);
                     }
                 }
 
@@ -222,10 +269,11 @@ public class InternetConnectivity {
                 double downloadSpeed, bytesPerSec, kilobytesPerSec, megabytesPerSec;
 
                 // Calculate download time by subtracting endTime from startTime
-                timeTakenMillis = Math.floor(endTime[0] - startTime);  // time taken in milliseconds
+                // time taken in milliseconds
+                timeTakenMillis = Math.floor(endTime[0] - startTime);
 
 
-                if (timeTakenMillis > 0){
+                if (timeTakenMillis > 0) {
                     // Network speed calculated
 
                     // Get time in seconds
@@ -264,12 +312,12 @@ public class InternetConnectivity {
                     connectionSpeedInfo.put("isFastNetwork", String.valueOf(isFastNetwork[0]));
 
                     // DEBUG Logs
-                    Log.d(TAG, "Time taken in milliseconds: " + timeTakenMillis );
-                    Log.d(TAG, "Time taken in seconds: " + timeTakenSecs );
-                    Log.d(TAG, "Bytes per sec: " + bytesPerSec );
-                    Log.d(TAG, "Kilobytes per sec: " + kilobytesPerSec );
-                    Log.d(TAG, "Megabytes per sec: " + megabytesPerSec );
-                    Log.d(TAG, "Download Speed: " + downloadSpeed );
+                    Log.d(TAG, "Time taken in milliseconds: " + timeTakenMillis);
+                    Log.d(TAG, "Time taken in seconds: " + timeTakenSecs);
+                    Log.d(TAG, "Bytes per sec: " + bytesPerSec);
+                    Log.d(TAG, "Kilobytes per sec: " + kilobytesPerSec);
+                    Log.d(TAG, "Megabytes per sec: " + megabytesPerSec);
+                    Log.d(TAG, "Download Speed: " + downloadSpeed);
                     Log.d(TAG, "Test file size: " + testFileSize[0]);
                 }
             }
@@ -280,6 +328,7 @@ public class InternetConnectivity {
 
     /**
      * Function to round double to 2 decimal places
+     *
      * @param value - double value
      */
     private static double roundDouble(double value) {
